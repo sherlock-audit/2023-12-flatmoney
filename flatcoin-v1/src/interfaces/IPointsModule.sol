e14da447aed1eeb25e6623e16a15231326a35a9a
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-18 16:29:25 +0100

    Delete scope directories

--- flatcoin-v1/src/interfaces/IPointsModule.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.18;
-
-interface IPointsModule {
-    function mintLeverageOpen(address to, uint256 size) external;
-
-    function mintDeposit(address to, uint256 depositAmount) external;
-}
