fd18ed907b011e9c94248ef69a557f01bc3728dd
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-19 21:04:16 +0100

    Delete scope directories

--- flatcoin-v1/lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "src/**/*.sol": ["prettier --write --plugin=prettier-plugin-solidity", "solhint --max-warnings 0"],
-  "test/**/*.sol": "prettier --write --plugin=prettier-plugin-solidity",
-  "{tasks,scripts}/**/*.{js,ts}": "prettier --write",
-};
